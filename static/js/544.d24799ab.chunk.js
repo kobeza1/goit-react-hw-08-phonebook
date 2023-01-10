7637f6187f49886cf353513681ec7d436bd13da7
kobeza1 <102040962+kobeza1@users.noreply.github.com>
2023-01-10 11:04:14 +0000

    Deploying to gh-pages from @ kobeza1/goit-react-hw-08-phonebook@83badc6be7d55513d1ca5971aa8edf12475df718 🚀

---
+++ static/js/544.d24799ab.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[544],{111:function(e,t,n){n.d(t,{$:function(){return i}});var o={section__title:"Section_section__title__5once"},c=n(329),i=function(e){var t=e.title,n=e.children;return(0,c.jsxs)("section",{className:o.section,children:[(0,c.jsx)("h2",{className:o.section__title,children:t}),n]})}},544:function(e,t,n){n.r(t);var o=n(111),c=n(329);t.default=function(){return(0,c.jsx)(o.$,{title:"Welcome to the PhoneBook App!"})}}}]);
+//# sourceMappingURL=544.d24799ab.chunk.js.map
